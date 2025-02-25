cd1218c943456eaae4222cd1fe00aa3d91e4f331
ilia.chernov <ilia.chernov@cecg.io>
2025-02-25 17:21:51 +0200

    Add a feature flag to disable sentiment analysis.

--- support-bot-api/src/main/java/com/coreeng/supportbot/sentiment/SentimentAnalysisJob.java
+++ support-bot-api/src/main/java/com/coreeng/supportbot/sentiment/SentimentAnalysisJob.java
@@ -5,9 +5,7 @@ import com.google.common.collect.ImmutableList;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 
-@Component
 @RequiredArgsConstructor
 @Slf4j
 public class SentimentAnalysisJob {
--- support-bot-api/src/main/java/com/coreeng/supportbot/sentiment/SentimentQueryService.java
+++ support-bot-api/src/main/java/com/coreeng/supportbot/sentiment/SentimentQueryService.java
@@ -2,11 +2,9 @@ package com.coreeng.supportbot.sentiment;
 
 import com.coreeng.supportbot.ticket.TicketId;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 import javax.annotation.Nullable;
 
-@Service
 @RequiredArgsConstructor
 public class SentimentQueryService {
     private final SentimentRepository repository;
