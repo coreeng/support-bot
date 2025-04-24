93eb108e87c4af1925a381b30b03818da25c92b0
ilia.chernov <ilia.chernov@cecg.io>
2025-04-24 17:16:22 +0300

    chore: test mend gh integration

--- api/service/src/main/java/com/coreeng/supportbot/teams/PlatformUser.java
+++ api/service/src/main/java/com/coreeng/supportbot/teams/PlatformUser.java
@@ -3,12 +3,14 @@ package com.coreeng.supportbot.teams;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 import java.util.Set;
 
 @Getter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @RequiredArgsConstructor
+@ToString(onlyExplicitlyIncluded = true)
 public class PlatformUser {
     @EqualsAndHashCode.Include
     private final String email;
