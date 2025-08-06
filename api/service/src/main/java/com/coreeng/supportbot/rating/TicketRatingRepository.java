dc35b16aea5b1cf6063ceb7a8b615b2e6f4a0553
Senna Semakula-Buuza <senna.semakula@sky.uk>
2025-08-06 15:17:57 +0100

    add rating package

---
+++ api/service/src/main/java/com/coreeng/supportbot/rating/TicketRatingRepository.java
@@ -0,0 +1,17 @@
+package com.coreeng.supportbot.rating;
+
+import javax.annotation.Nullable;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.UUID;
+
+public interface TicketRatingRepository {
+    
+    UUID insertRating(TicketRating rating);
+    
+    @Nullable TicketRating findById(UUID id);
+    ImmutableList<TicketRating> findRatingsByStatus(String status);
+    ImmutableList<TicketRating> findRatingsByTag(String tag);
+    ImmutableList<TicketRating> findEscalatedRatings();
+}
