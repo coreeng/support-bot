63c9fedd449233622923f61cf8d24452ed048bf0
Senna Semakula-Buuza <senna.semakula@sky.uk>
2025-08-06 15:18:53 +0100

    refactor ticket rating service

---
+++ api/service/src/main/java/com/coreeng/supportbot/ticket/handler/TicketRatingEventHandler.java
@@ -0,0 +1,50 @@
+package com.coreeng.supportbot.ticket.handler;
+
+import com.coreeng.supportbot.rating.TicketRatingService;
+import com.coreeng.supportbot.slack.client.SlackClient;
+import com.coreeng.supportbot.slack.client.SlackGetMessageByTsRequest;
+import com.coreeng.supportbot.ticket.TicketRepository;
+import com.coreeng.supportbot.ticket.TicketStatus;
+import com.coreeng.supportbot.ticket.TicketStatusChanged;
+import com.coreeng.supportbot.ticket.slack.TicketSlackService;
+import com.slack.api.model.Message;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TicketRatingEventHandler {
+    private final TicketSlackService slackService;
+    private final TicketRepository ticketRepository;
+    private final SlackClient slackClient;
+
+    @EventListener
+    public void onTicketStatusChange(TicketStatusChanged event) {
+        if (event.status() == TicketStatus.closed) {
+            // Trigger rating collection when ticket is closed
+            var ticket = ticketRepository.findTicketById(event.ticketId());
+            if (ticket != null) {
+                log.info("Ticket {} closed, posting rating request", event.ticketId());
+                
+                try {
+                    // Get the original query message to find the user who created the ticket
+                    var originalMessage = slackClient.getMessageByTs(SlackGetMessageByTsRequest.of(ticket.queryRef()));
+                    
+                    String userId = originalMessage.getUser();
+                    if (userId != null) {
+                        slackService.postRatingRequest(ticket.queryRef(), event.ticketId(), userId);
+                    } else {
+                        log.warn("Could not determine user for ticket {} rating request", event.ticketId());
+                    }
+                } catch (Exception e) {
+                    log.error("Error getting original message for ticket {} rating request", event.ticketId(), e);
+                }
+            } else {
+                log.warn("Could not find ticket {} to post rating request", event.ticketId());
+            }
+        }
+    }
+}
