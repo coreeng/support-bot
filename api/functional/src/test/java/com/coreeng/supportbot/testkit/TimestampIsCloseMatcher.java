90f5aed4832c5e87651bddcc9bafbb8c780a1b83
ilia.chernov <ilia.chernov@cecg.io>
2025-09-23 17:03:21 +0100

    test: Get rid of test flakiness that was caused by the difference in 1 seconds between asserted and actual timestamp.
    The reason for this is that the asserted timestamp we create in the process with tests and actual is created in the process under test. Sometimes it causes failures

---
+++ api/functional/src/test/java/com/coreeng/supportbot/testkit/TimestampIsCloseMatcher.java
@@ -0,0 +1,30 @@
+package com.coreeng.supportbot.testkit;
+
+import net.javacrumbs.jsonunit.core.ParametrizedMatcher;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+
+import java.math.BigDecimal;
+
+public class TimestampIsCloseMatcher extends BaseMatcher<Object> implements ParametrizedMatcher {
+    private long timestamp;
+
+    @Override
+    public void setParameter(String parameter) {
+        this.timestamp = Long.parseLong(parameter);
+    }
+
+    @Override
+    public boolean matches(Object actual) {
+        if (actual instanceof BigDecimal actualNum) {
+            return Math.abs(actualNum.longValueExact() - timestamp) <= 1;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("timestamp is +-1 compared to ").appendValue(timestamp);
+    }
+}
