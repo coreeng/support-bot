45b553ec7b1bfcfb4f7c6771f37bcd4ffa968a69
Savvas Michael <savvas.michael@sky.uk>
2025-10-03 11:02:50 +0300

    feat:Removing the UI directory and refactor base Makefile

--- ui/plugins/support-bot/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { cecgSupportBotPlugin, CecgSupportBotPage } from './plugin';
--- ui/plugins/support-bot/src/state/index.ts
+++
@@ -1,6 +0,0 @@
-import { useTickets } from './useTickets';
-import { useTeams } from './useTeams';
-import { useEscalations } from './useEscalation';
-import { useStats } from './useStats';
-
-export { useTickets, useTeams, useEscalations, useStats };
