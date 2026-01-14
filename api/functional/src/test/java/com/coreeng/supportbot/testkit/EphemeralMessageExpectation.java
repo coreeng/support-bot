35da7c1ad81589826bcf82ab112191d6a15c84bc
ilia.chernov <ilia.chernov@cecg.io>
2026-01-14 11:17:53 +0000

    chore(functional-tests): Verify rating request message

--- api/functional/src/test/java/com/coreeng/supportbot/testkit/EphemeralMessageExpectation.java
+++ api/functional/src/test/java/com/coreeng/supportbot/testkit/EphemeralMessageExpectation.java
@@ -1,13 +1,18 @@
 package com.coreeng.supportbot.testkit;
 
 import lombok.Builder;
+import lombok.Getter;
 import org.jspecify.annotations.NonNull;
 
 @Builder
-public record EphemeralMessageExpectation(
-    @NonNull String channelId,
-    @NonNull MessageTs threadTs,
-    @NonNull String userId
-) {
+@Getter
+public class EphemeralMessageExpectation<T> {
+    @NonNull
+    private final String channelId;
+    @NonNull
+    private final MessageTs threadTs;
+    @NonNull
+    private final String userId;
+    private final StubWithResult.@NonNull Receiver<T> receiver;
 }
 
