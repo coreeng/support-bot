781e23df1a50feabff9cbce79d9fd9f9036dd588
ilia.chernov <ilia.chernov@cecg.io>
2026-04-01 14:51:09 +0100

    refactor(api): extract team suggestion orchestration from controller to service

--- api/service/src/main/java/com/coreeng/supportbot/ticket/rest/TicketTeamSuggestionsUI.java
+++ api/service/src/main/java/com/coreeng/supportbot/ticket/rest/TicketTeamSuggestionsUI.java
@@ -1,5 +1,11 @@
 package com.coreeng.supportbot.ticket.rest;
 
+import com.coreeng.supportbot.ticket.TicketTeamsSuggestion;
 import com.google.common.collect.ImmutableList;
 
-public record TicketTeamSuggestionsUI(ImmutableList<String> suggestedTeams, ImmutableList<String> otherTeams) {}
+public record TicketTeamSuggestionsUI(ImmutableList<String> suggestedTeams, ImmutableList<String> otherTeams) {
+
+    public static TicketTeamSuggestionsUI from(TicketTeamsSuggestion suggestion) {
+        return new TicketTeamSuggestionsUI(suggestion.userTeams(), suggestion.otherTeams());
+    }
+}
