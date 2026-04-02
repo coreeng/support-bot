fd4ea2ef21273187a0855111e40393e0517ceddd
Oleg Proudnikov <oleg.proudnikov@sky.uk>
2026-04-02 15:04:12 +0100

    Restore support for Any Date in date picker (#213)
    * Restore support for Any Date in date picker
    
    * Maker Last Week default choice

--- ui/src/components/escalations/__tests__/escalations.test.tsx
+++ ui/src/components/escalations/__tests__/escalations.test.tsx
@@ -423,9 +423,9 @@ describe('EscalationsPage', () => {
             expect(screen.queryByText('T-old')).not.toBeInTheDocument()
         })
 
-        it('shows all escalations when date filter is cleared', () => {
+        it('shows all escalations when Any Date is selected', () => {
             render(<EscalationsPage />, { wrapper: Wrapper })
-            fireEvent.change(screen.getByTestId('escalations-date-filter'), { target: { value: '' } })
+            fireEvent.change(screen.getByTestId('escalations-date-filter'), { target: { value: 'all' } })
             expect(screen.getByText('T-recent')).toBeInTheDocument()
             expect(screen.getByText('T-old')).toBeInTheDocument()
         })
