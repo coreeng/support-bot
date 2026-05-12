1a1fcf7c3fa24e95f86fabedbf911b7324e98d3d
ilia.chernov <ilia.chernov@cecg.io>
2026-05-12 18:40:19 +0100

    test change

--- api/service/src/main/java/com/coreeng/supportbot/util/Page.java
+++ api/service/src/main/java/com/coreeng/supportbot/util/Page.java
@@ -5,6 +5,9 @@ import static com.google.common.collect.ImmutableList.toImmutableList;
 import com.google.common.collect.ImmutableList;
 import java.util.function.Function;
 
+// test change: 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
+// test change 5
+
 public record Page<T>(ImmutableList<T> content, long page, long totalPages, long totalElements) {
     public <Y> Page<Y> map(Function<T, Y> mapperFn) {
         return new Page<>(content.stream().map(mapperFn).collect(toImmutableList()), page, totalPages, totalElements);
